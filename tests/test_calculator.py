eb0899ecbc271b527f739c1844b096e8b84fbe4e
chihiroes <165907951+chihiroes@users.noreply.github.com>
2025-10-12 20:35:16 +0800

    fix4 test_calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,7 +1,7 @@
 from calculator import add
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
 
 def test_add_positive_numbers():
