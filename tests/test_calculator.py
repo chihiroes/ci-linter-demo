9545c557dc29aba8b3bc56bfda43741222ad8922
chihiroes <2993451764@qq.com>
2025-10-12 21:17:44 +0800

    add comment to test_cal.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,9 +2,9 @@ from calculator import add, subtract
 import sys
 import os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
-
-
 """"加法测试"""
+
+
 def test_add_positive_numbers():
     assert add(2, 3) == 5
 
@@ -29,9 +29,9 @@ def test_add_float_numbers():
 
 def test_add_large_numbers():
     assert add(1000000, 2000000) == 3000000
-
-
 """减法测试"""
+
+
 def test_subtract_positive_numbers():
     assert subtract(5, 3) == 2
 
